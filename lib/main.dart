84eaeadc73f41f0f230e55f20e15f7a35f77f5ee
ottojoash <louisjoshbricks@gmail.com>
2022-11-16 13:43:00 -0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MainWidget extends StatefulWidget {
 }
 
 class _MainWidgetState extends State<MainWidget> with TickerProviderStateMixin {
-  KFDrawerController _drawerController;
+  late KFDrawerController _drawerController;
 
   @override
   void initState() {
